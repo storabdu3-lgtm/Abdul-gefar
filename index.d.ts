334c7bf05fcd522783b095e5a149a9df1c3c9a9d
storabdu3-lgtm <storabdu3@gmail.com>
2026-08-12 01:07:53 -0700

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,5 @@
-export {};
+export * from "./generated/api";
+export * from "./generated/api.schemas";
+export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
+export type { AuthTokenGetter } from "./custom-fetch";
 //# sourceMappingURL=index.d.ts.map
